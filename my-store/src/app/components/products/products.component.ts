981278ecf0cef55d9974e7cac696dd705993e18a
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-02-15 12:02:55 -0700

    obteniendo datos de una API

--- my-store/src/app/components/products/products.component.ts
+++ my-store/src/app/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { StoreService } from "../../services/store.service";
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -13,52 +14,20 @@ export class ProductsComponent implements OnInit {
 
   total:number = 0
 
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'El mejor juguete',
-      image: './assets/images/toy.jpg',
-      price: 565
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      image: './assets/images/bike.jpg',
-      price: 356
-    },
-    {
-      id: '3',
-      name: 'Collecion de albumes',
-      image: './assets/images/album.jpg',
-      price: 134
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      image: './assets/images/books.jpg',
-      price: 23
-    },
-    {
-      id: '5',
-      name: 'Casa para perro',
-      image: './assets/images/house.jpg',
-      price: 34
-    },
-    {
-      id: '6',
-      name: 'Gafas',
-      image: './assets/images/glasses.jpg',
-      price: 3434
-    },
-  ]
+  products: Product[] = []
 
   constructor(
-    private storeService: StoreService
+    private storeService: StoreService,
+    private productsService: ProductsService
   ) {
     this.myShoppingCart = this.storeService.getShoppingCart()
   }
 
   ngOnInit(): void {
+    this.productsService.getAllProducts()
+    .subscribe(data => {
+      this.products = data
+    })
   }
 
   onAddToShoppingCart(product:Product) {
